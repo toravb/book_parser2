4989cee2f580a76e438e096339bfbdfef338600d
antonwebnauts <antonwebnauts@gmail.com>
2022-01-06 18:32:02 +0200

    Make surname and avatar nullable

--- database/migrations/2022_01_04_150354_add_surname_to_users.php
+++ database/migrations/2022_01_04_150354_add_surname_to_users.php
@@ -9,7 +9,7 @@ class AddSurnameToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('surname');
+            $table->string('surname')->nullable();
         });
     }
 
--- database/migrations/2022_01_06_123226_add_avatar_to_users.php
+++ database/migrations/2022_01_06_123226_add_avatar_to_users.php
@@ -14,7 +14,7 @@ class AddAvatarToUsers extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('avatar');
+            $table->string('avatar')->nullable();
         });
     }
 
