9227214c3e3af181923da413cb53defa2708bfb6
Gladyr <gladyrandrew@gmail.com>
2022-01-20 15:27:27 +0200

    authorPage2

--- app/Api/Http/Controllers/ProfileController.php
+++ app/Api/Http/Controllers/ProfileController.php
@@ -2,12 +2,9 @@
 
 namespace App\Api\Http\Controllers;
 
-use App\Api\Models\UserSettings;
-use App\Api\Services\ApiAnswerService;
 use App\Api\Services\UserService;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\DB;
 
 
 class ProfileController extends Controller
@@ -16,12 +13,7 @@ class ProfileController extends Controller
     {
 
         $user = Auth::user();
-        $userSettings = UserSettings:: where('user_id', $user->id)
-            ->select('likes','commented', 'commentedOthers')->first();
 
-        $user->user_settings = $userSettings;
-
-        return ApiAnswerService::successfulAnswerWithData($user);
-
+        return UserService::userAvatar($user);
     }
 }
