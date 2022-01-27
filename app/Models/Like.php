91a0bfd465670e015073c82bbfecbb97ba6129a2
Gladyr <gladyrandrew@gmail.com>
2022-01-27 13:28:52 +0200

    Comments init Like changes

--- app/Models/Like.php
+++ app/Models/Like.php
@@ -2,12 +2,29 @@
 
 namespace App\Models;
 
+use App\Api\Interfaces\Types;
+use http\Env\Request;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Auth;
 
 class Like extends Model
 {
+
     public function likeable()
     {
         return $this->morphTo();
     }
+
+    public function addlike(Request $request)
+    {
+        $user = Auth::user();
+        $params = [
+            'user_id' => $user->id,
+            'likeable_id' => $request->id,
+            'likeable_type' => get_class($request)];
+        Like::firstOrCreate($params);
+        return back();
+    }
+
+
 }
