fb34a7902dfb307a27df2f0017110dd3b0c863f5
Alexander Krysan <krysan1986@gmail.com>
2022-02-10 12:20:51 +0200

    add genres output

--- app/Api/Http/Controllers/MainPageController.php
+++ app/Api/Http/Controllers/MainPageController.php
@@ -12,16 +12,21 @@ class MainPageController extends Controller
 {
     const COMPILATION_PAGINATION = 3;
     const REVIEWS_PAGINATION = 3;
+    const GENRES_PAGINATION = 13;
 
-    public function home(Compilation $compilation, AudioBook $audioBook, BookReview $review): \Illuminate\Http\JsonResponse
+    public function home(Compilation $compilation, AudioBook $audioBook, BookReview $review, CategoryController $categoryController): \Illuminate\Http\JsonResponse
     {
+        $genres = $categoryController->show();
+
         $compilations = $compilation->withSumAudioAndBooksCount();
 
         $audioBooksList = $audioBook->mainPagePaginateList();
 
         $mainPageReview = $review->latestReviewBookUser();
 
+//        return ApiAnswerService::successfulAnswerWithData($genres);
         return ApiAnswerService::successfulAnswerWithData([
+            'genres' => $genres,
             'compilations' => $compilations,
             'audioBooksList' => $audioBooksList,
             'reviews' => $mainPageReview
