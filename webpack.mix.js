f52d95b25a25d70303493569492b0866ab6082d6
AlexStrilBy <alex.stril.by@gmail.com>
2022-03-03 22:22:44 +0200

    app.css returned webpack compile changed

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,7 +14,12 @@ const mix = require('laravel-mix');
 mix.js('resources/js/app.js', 'public/js')
 mix.js('resources/js/admin.js', 'public/js')
 
-mix.postCss('resources/css/admin.css', 'public/css', [
-    require('postcss-import'),
-    require('autoprefixer'),
-]);
+mix.postCss('resources/css/admin.css', 'public/css');
+mix.postCss('resources/css/app.css', 'public/css');
+
+mix.options({
+    postCss: [
+        require('postcss-import'),
+        require('autoprefixer'),
+    ]
+});
