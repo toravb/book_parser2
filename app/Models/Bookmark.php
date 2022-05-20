2b30ea8b2bd829e677d7b4822d536d3243135c87
Alexander <krysan1986@gmail.com>
2022-05-20 11:39:35 +0300

    data display after saving bookmark formatted added authorization to GET bookmarks code refactor in BookmarksController.php docs corrected

--- app/Models/Bookmark.php
+++ app/Models/Bookmark.php
@@ -3,40 +3,57 @@
 namespace App\Models;
 
 use App\Http\Requests\BookmarkRequest;
-use http\Env\Request;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Support\Facades\Auth;
-use Staudenmeir\EloquentHasManyDeep\HasRelationships;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Bookmark extends Model
 {
-
     protected $fillable = ['book_id', 'user_id', 'page_id'];
 
-    public function book(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function book(): BelongsTo
     {
         return $this->belongsTo(Book::class);
     }
 
-    public function users(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function users(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function page(): \Illuminate\Database\Eloquent\Relations\BelongsTo
+    public function page(): BelongsTo
     {
         return $this->belongsTo(Page::class);
     }
 
+    public function chapter(): BelongsTo
+    {
+        return $this->belongsTo(Chapter::class, 'book_id', 'book_id');
+    }
+
     public function addGetBookmark(BookmarkRequest $request): Bookmark
     {
-        return Bookmark::firstOrCreate([
+        $userBookmark = Bookmark::firstOrCreate([
             'book_id' => $request->book_id,
             'user_id' => \auth()->id(),
             'page_id' => $request->page_id,
         ]);
+
+        $page = $userBookmark
+            ->page()
+            ->select(['id', 'page_number'])
+            ->get();
+
+        $userBookmark->chapter = $userBookmark
+            ->chapter()
+            ->addSelect('chapters.*', 'page_id', 'chapters.book_id', 'title', 'pages.id', 'pages.page_number')
+            ->join('pages', 'chapters.page_id', '=', 'pages.id')
+            ->where('pages.page_number', '<=', $page[0]['page_number'])
+            ->orderBy('pages.page_number', 'desc')
+            ->addSelect('chapters.id')
+            ->first();
+
+        $userBookmark->page = $page;
+
+        return $userBookmark;
     }
-
 }
-
-
