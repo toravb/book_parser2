2deef2a8d87b3ed7e01aa9f0302255e43a9d892d
Viktor <d3nydb@gmail.com>
2022-06-08 15:00:51 +0300

    make command for public_path in images

---
+++ app/Console/Commands/SetImagePublicPath.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Jobs\Audio\SetAudioPathJob;
+use App\Models\AudioAudiobook;
+use App\Models\AudioImage;
+use App\Models\Image;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
+
+class SetImagePublicPath extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'books:set-image-path';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Set public path for images';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        Image::query()->whereNull('public_path')->chunk(1000, function ($images){
+            foreach ($images as $image){
+                if ($image->link){
+                    $name = explode('/', $image->link);
+                    $disk = Storage::disk('book');
+                    if ($disk->exists(@end($name))){
+                        $path = url('img/photo_books/'.@end($name));
+                        dd($path);
+                        try {
+                            DB::transaction(function () use ($image, $path){
+                                $image->public_path = $path;
+                                $image->save();
+                            }, 3);
+                        }catch (\Exception $exception){
+                            dd($exception->getMessage());
+                        }
+                        echo $image->id.' - [OK]'."\n";
+                    }
+                }
+            }
+        });
+        echo '[COMPLETE]';
+        return 0;
+    }
+}
